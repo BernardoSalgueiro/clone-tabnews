717be11dbb6da395f2da1b26bf94c94b887111ab
Bernardo Guimarães Salgueiro <bernardogsalgueiro11@gmail.com>
2026-05-26 20:52:04 +0000

    adição de aspas na frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O sucesso ocorre quando a sua coragem é maior que os seus medos.</h1>
+    return <h1>"O sucesso ocorre quando a sua coragem é maior que os seus medos."</h1>
 }
 
 export default Home;
